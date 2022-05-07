0e793500ecaef6c25f4137947e62bf7b0c0df3b2
huequica <dev@huequica.xyz>
2022-05-08 02:22:00 +0900

    implement function that return is file exist

--- src/repositories/prettier.ts
+++ src/repositories/prettier.ts
@@ -1,6 +1,19 @@
 import type { Options } from 'prettier';
 import fs from 'fs/promises';
 
+/**
+ * ファイルの存在を確認する
+ * @param filepath 確認対象のファイルパス 実行時の環境の相対パスであることに注意
+ * @return 存在すれば true それ以外が false
+ */
+const isFileExists = async (filepath: string): Promise<boolean> => {
+  try {
+    return Boolean(await fs.lstat(filepath));
+  } catch (_) {
+    return false;
+  }
+};
+
 export class PrettierRcRepository {
   public option: Options = {
     semi: true,
