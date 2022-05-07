3a3f653545d9c45f98b176f07a8525b3111316ac
huequica <dev@huequica.xyz>
2022-05-08 02:04:49 +0900

    remove default test file

--- src/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { main } from '@/index';
-
-describe('main', () => {
-  it('run console.log', () => {
-    const spyOfLog = jest.spyOn(console, 'log').mockImplementation();
-    main();
-    expect(spyOfLog).toBeCalled();
-  });
-});
