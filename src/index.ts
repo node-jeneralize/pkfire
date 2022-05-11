d711b4c12f6b8f1c8cefcdd4e15890a8cdb3f084
huequica <dev@huequica.xyz>
2022-05-12 03:35:15 +0900

    create main command story

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export const main = () => {
-  console.log('write codes!');
+import { cac } from 'cac';
+import { isNodeOrFront } from '@/questions/isNodeOrFront';
+
+export const main = async () => {
+  const cli = cac('project_kicker');
+
+  cli
+    .command('', 'generate node toolChain files, install modules')
+    .action(async () => {
+      console.log(await isNodeOrFront());
+    });
+
+  cli.parse();
+  cli.help();
 };
+
+main();
