8b6e9535ea5f82daff99ddb7b163940f8d0601af
huequica <dev@huequica.xyz>
2022-05-15 04:06:04 +0900

    create package manager question

--- src/commands/general.ts
+++ src/commands/general.ts
@@ -1,12 +1,12 @@
 import { askUseTypeScript } from '@/questions/useTypeScript';
 import { askLinterAndFormatter } from '@/questions/linterAndFormatter';
+import { askWhichPackageManager } from '@/questions/packageManager';
 
 /**
  * パラメータなどの引数なしで実行したときの挙動を実行する
  */
 export const runGeneralCommandJob = async () => {
+  const packageManager = await askWhichPackageManager();
   const environment = await askUseTypeScript();
   const linterAndFormatter = await askLinterAndFormatter();
-  console.log('environment: ', environment);
-  console.log('linterAndFormatter', linterAndFormatter);
 };
