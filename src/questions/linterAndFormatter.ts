f1e17bac75bb351acebce7b3060dbf01dd29b06c
huequica <dev@huequica.xyz>
2022-06-16 22:59:25 +0900

    add jest to using toolchains prompt

--- src/questions/linterAndFormatter.ts
+++ src/questions/linterAndFormatter.ts
@@ -1,14 +1,16 @@
-import inquirer, { QuestionCollection } from 'inquirer';
+import inquirer, { QuestionCollection, Separator } from 'inquirer';
 import { ESLintRc } from '@/repositories/eslint';
 import { PrettierRc } from '@/repositories/prettier';
+import { Jest } from '@/repositories/jest';
 
 interface PromptAnswer {
-  LinterAndFormatter: Array<'eslint' | 'prettier'>;
+  LinterAndFormatter: Array<'eslint' | 'prettier' | 'jest'>;
 }
 
 interface ReturnObject {
   ESLint: ESLintRc | undefined;
   Prettier: PrettierRc | undefined;
+  Jest: Jest | undefined;
 }
 /**
  * Linter や Formatter を使うかどうか質問する
@@ -21,6 +23,7 @@ export const askLinterAndFormatter = async (): Promise<ReturnObject> => {
       name: 'LinterAndFormatter',
       message: 'What do you want to use?',
       choices: [
+        new Separator(' == Code Styles, Linting'),
         {
           name: 'ESLint',
           value: 'eslint',
@@ -28,6 +31,11 @@ export const askLinterAndFormatter = async (): Promise<ReturnObject> => {
         {
           name: 'Prettier',
           value: 'prettier',
+        },
+        new Separator(' == Unit(E2E) Testing'),
+        {
+          name: 'Jest',
+          value: 'jest',
         },
       ],
     },
@@ -44,8 +52,12 @@ export const askLinterAndFormatter = async (): Promise<ReturnObject> => {
     ? new PrettierRc()
     : undefined;
 
+  // Jest の選択肢が有効であれば jest の設定クラスを生成
+  const jest = userResponse.includes('jest') ? new Jest() : undefined;
+
   return {
     ESLint: eslintrc,
     Prettier: prettierrc,
+    Jest: jest,
   };
 };
