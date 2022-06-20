9e140bc5903dbbd7216cb794d5355b127da06938
huequica <dev@huequica.xyz>
2022-06-20 13:46:09 +0900

    split type definition

--- src/repositories/core/toolchain.ts
+++ src/repositories/core/toolchain.ts
@@ -1,10 +1,14 @@
+export type Dependencies = {
+  [key: 'always' | string]: string | string[];
+};
+
 /**
  * 依存ツールを管轄するクラスの interface
  */
 export interface Toolchain {
   // 依存プラグインの定義
   // always というキーは最低限必須,あとは子のクラスによる
-  dependencies: { [key: 'always' | string]: string | string[] };
+  dependencies: Dependencies;
 
   // 設定のデータ
   config?: unknown;
